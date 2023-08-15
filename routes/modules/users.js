07ca5845c2c3a58cd037b9a61a658e6334f8b79d
Verna0214 <yuhsichen0214@gmail.com>
2023-08-15 15:52:44 +0800

    feat: add user register post router

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 // login
 router.get('/login', (req, res) => {
@@ -13,6 +14,29 @@ router.get('/register', (req, res) => {
 
 // login post
 router.post('/login', (req, res) => {
+})
+
+// register post
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  if (password !== confirmPassword) {
+    console.log('confirmPassword is not correct.')
+    return res.redirect('/')
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        console.log('User is already exist.')
+        res.render('register', { name, email, password, confirmPassword })
+      } else {
+        return User.create({ name, email, password })
+          .then(() => res.redirect('/'))
+          .catch(error => console.log(error))
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 
