5d1dc22e4fee1e56ff457f6a5906915f20184a60
Verna0214 <yuhsichen0214@gmail.com>
2023-08-17 16:14:41 +0800

    modify: todoSeeder.js

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,38 @@
+const bcrypt = require('bcryptjs')
+// 判斷執行環境載入 dotenv 環境變數
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
+const User = require('../user')
 const Todo = require('../todo')
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 // 判斷連線成功
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` })
-  }
-  console.log('done!')
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then((user) => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({ name: `name-${i}`, userId })
+      ))
+    })
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
 })
